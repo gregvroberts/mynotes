38f198f1fc17b2c6bdf7a9fbd63ee67efe2cf21a
Gregory Roberts <roberts.gregory@heb.com>
2021-05-27 17:33:47 -0500

    Initial Commit

---
+++ src/main/java/com/greg/mynotes/MynotesApplication.java
@@ -0,0 +1,13 @@
+package com.greg.mynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greg/mynotes/MynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greg.mynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
